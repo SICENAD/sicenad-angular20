361fa9941e5a3263d6a332cc1bbcbbe8e1fc6873
joseluispuentes82 <joseluispuentes@hotmail.com>
2025-09-04 20:00:06 +0200

    cenad modal con reactiveforms funcionando

--- sicenad-angular20/src/app/cenads/components/cenadModal/cenadModal.component.ts
+++ sicenad-angular20/src/app/cenads/components/cenadModal/cenadModal.component.ts
@@ -1,4 +1,5 @@
-import { Component, computed, effect, inject, input, output, signal } from '@angular/core';
+import { Component, computed, effect, ElementRef, inject, input, output, signal, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { Cenad } from '@interfaces/models/cenad';
 import { OrquestadorService } from '@services/orquestadorService';
@@ -8,7 +9,7 @@ import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-cenad-modal',
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, ReactiveFormsModule],
   templateUrl: './cenadModal.component.html',
   styleUrls: ['./cenadModal.component.css'],
 })
@@ -16,6 +17,8 @@ export class CenadModalComponent {
   private utils = inject(UtilsStore);
   private orquestadorService = inject(OrquestadorService);
   private iconos = inject(IconosStore);
+  private fb = inject(FormBuilder);
+
   faEdit = this.iconos.faEdit;
   // --- Inputs / Outputs ---
   cenad = input<Cenad>();
@@ -24,138 +27,119 @@ export class CenadModalComponent {
   // --- State ---
   provincias = signal<{ idProvincia: number, nombre: string }[]>(this.utils.provincias());
   sizeMaxEscudo = computed(() => this.utils.sizeMaxEscudo());
-
-  nombre = signal<string>(this.cenad()?.nombre || '');
-  direccion = signal<string>(this.cenad()?.direccion || '');
-  provinciaSeleccionada = signal<number>(this.cenad()?.provincia || 0);
-  tfno = signal<string>(this.cenad()?.tfno || '');
-  email = signal<string>(this.cenad()?.email || '');
-  descripcion = signal<string>(this.cenad()?.descripcion || '');
-  escudoActual = signal<string>(this.cenad()?.escudo || '');
-  urlEscudoActual = signal<string>(this.cenad()?.descripcion || '');
-  archivoEscudo = signal<File | null>(null);
-  previewEscudo = signal<string>('');
-  idCenad = signal<string>(this.cenad()?.idString || '');
-
+  idCenad = computed(() => this.cenad()?.idString || '');
   _idModal = signal('modal-cenad-' + this.cenad()?.idString);
   _idModalEliminar = signal('modal-cenad-eliminar-' + this.cenad()?.idString);
   idModal = computed(() => this._idModal() + this.idCenad());
   idModalEliminar = computed(() => this._idModalEliminar() + this.idCenad());
-  provincia = computed(() => this.cenad()?.provincia || 0);
-  provinciaSeleccionadaNombre = computed(() => {
-    return this.provincias().find(p => p.idProvincia === this.provinciaSeleccionada())?.nombre || '';
+  @ViewChild('fileInput') fileInput!: ElementRef<HTMLInputElement>;
+
+  previewEscudo = signal<string>('');
+  escudoActual = signal<string>((this.cenad()?.escudo || ''));
+  urlEscudoActual = signal<string>('');
+  escudoFile = signal<File | null>(null);
+  cenadForm: FormGroup = this.fb.group({
+    nombre: ['', Validators.required],
+    provincia: [0, [Validators.required, Validators.min(1)]],
+    direccion: ['', Validators.required],
+    tfno: ['', [Validators.required, Validators.pattern(/^[0-9]{9}$/)]],
+    email: ['', [Validators.required, Validators.email]],
+    descripcion: ['', Validators.required],
+    escudo: [null] // archivo opcional, se reemplaza si se carga uno nuevo
   });
-
-  ngOnInit(): void {
-    if (!this.cenad()) return;
-
-  }
-  private syncCenad = effect(() => {
-    const c = this.cenad();
-    if (!c) return;
-    this.nombre.set(c.nombre);
-    this.provinciaSeleccionada.set(c.provincia);
-    this.direccion.set(c.direccion);
-    this.tfno.set(c.tfno);
-    this.email.set(c.email);
-    this.descripcion.set(c.descripcion);
-    this.escudoActual.set(c.escudo);
-    this.idCenad.set(c.idString);
-  });
-
 
   private esperarYCargarEscudo = void effect(() => {
+    this.escudoActual.set(this.cenad()?.escudo || '');
     const escudo = this.cenad()?.escudo;
     if (!escudo) return;
     if (!this.idCenad()) return;
-    this.orquestadorService.getEscudoCenad(escudo, this.idCenad()).subscribe({
-      next: blob => this.urlEscudoActual.set(URL.createObjectURL(blob)),
-      error: err => console.error('Error cargando escudo', err)
-    });
+    this.orquestadorService.getEscudoCenad(escudo, this.idCenad()).subscribe(
+      {
+        next: blob => this.urlEscudoActual.set(URL.createObjectURL(blob)),
+        error: err => console.error('Error cargando escudo', err)
+      });
   });
 
+
+
+  ngOnInit(): void {
+    if (this.cenad()) {
+      this.cenadForm.patchValue({
+        nombre: this.cenad()?.nombre,
+        provincia: this.cenad()?.provincia,
+        direccion: this.cenad()?.direccion,
+        tfno: this.cenad()?.tfno,
+        email: this.cenad()?.email,
+        descripcion: this.cenad()?.descripcion,
+      });
+
+    }
+  }
+
+  get nombre() { return this.cenadForm.get('nombre'); }
+  get provincia() { return this.cenadForm.get('provincia'); }
+  get direccion() { return this.cenadForm.get('direccion'); }
+  get tfno() { return this.cenadForm.get('tfno'); }
+  get email() { return this.cenadForm.get('email'); }
+  get descripcion() { return this.cenadForm.get('descripcion'); }
+  get escudo() { return this.cenadForm.get('escudo'); }
+
   onFileChange(event: any) {
-    const file = event.target.files[0];
-    if (!file) return;
-    this.archivoEscudo.set(file);
-
-    // Generamos preview
-    const reader = new FileReader();
-    reader.onload = e => this.previewEscudo.set((e.target?.result as string) || '');
-    reader.readAsDataURL(file);
+    const file: File = event.target.files[0];
+    if (file) {
+      this.cenadForm.patchValue({ escudo: file });
+      const reader = new FileReader();
+      reader.onload = e => this.previewEscudo.set(e.target?.result as string)
+      reader.readAsDataURL(file);
+      this.escudoFile.set(file);
+    } else {
+      this.escudoFile.set(null);
+    }
   }
 
-  onProvinciaChange(e: Event) {
-    const value = (e.target as HTMLSelectElement).value; // string
-    // Convierte a número y evita NaN en caso de opción vacía
-    this.provinciaSeleccionada.set(Number(value));
-  }
-  provinciaSeleccionadaString = computed(() => this.provinciaSeleccionada()?.toString() || '');
+  editarCenad() {
+    if (this.cenadForm.invalid) {
+      this.cenadForm.markAllAsTouched();
+      return;
+    }
+    const { nombre, provincia, direccion, tfno, email, descripcion } = this.cenadForm.value;
+    const archivoEscudo = this.escudoFile();
+    console.log('Archivo nuevo:', archivoEscudo);
+    console.log('Escudo actual:', this.escudoActual());
 
-
-  async editarCenad() {
-    const provinciaId: number = this.provinciaSeleccionada();
-    let nombreArchivo = '';
     this.orquestadorService.actualizarCenad(
-      this.nombre(),
-      provinciaId,
-      this.direccion(),
-      this.tfno(),
-      this.email(),
-      this.descripcion(),
-      this.archivoEscudo(),
-      this.escudoActual(),
+      nombre,
+      provincia,
+      direccion,
+      tfno,
+      email,
+      descripcion,
+      archivoEscudo,             // archivo opcional
+      this.escudoActual(),  // archivo anterior
       this.idCenad()
-    ).subscribe(res => {
-      if (res) {
-        nombreArchivo = res;
-        console.log(`Cenad ${this.nombre()} actualizado correctamente.`);
+    ).subscribe({
+      next: res => {
+        if (res) {
+          console.log(`Cenad ${nombre} actualizado correctamente.`);
+          this.escudoActual.set(res);
+          this.cenad()!.escudo = this.escudoActual(); // actualizamos el escudo en el objeto cenad
+          this.cenadForm.patchValue({ escudo: null });
+          this.previewEscudo.set('');
+          this.escudoFile.set(null);
+          if (this.fileInput) this.fileInput.nativeElement.value = '';
+        }
+        this.output.emit(); // notificamos al padre
+      },
+      error: (err) => {
+        console.error('Error actualizando Cenad:', err);
       }
-        console.log('Cenad actualizado correctamente.' );
-
-    if (nombreArchivo != '') {
-      this.escudoActual.set(nombreArchivo);
-      this.archivoEscudo.set(null);
-      this.previewEscudo.set('');
-    }})
-
-    this.output.emit(); // Notificamos al componente padre
+    });
   }
 
-  async borrarCenad() {
-    this.orquestadorService.borrarCenad(this.idCenad()).subscribe();
-    this.output.emit(); // Notificamos al componente padre
+  borrarCenad() {
+    this.orquestadorService.borrarCenad(this.idCenad()).subscribe(() => {
+      this.output.emit(); // notificamos al padre
+    });
   }
 
-  // Validaciones
-  isValidEmail(email: string): boolean {
-    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-  }
-
-  isValidPhone(phone: string): boolean {
-    return /^[0-9]{9}$/.test(phone);
-  }
-
-  emailError(): string | null {
-    if (!this.email().trim()) return null;
-    return this.isValidEmail(this.email()) ? null : 'El correo no es válido';
-  }
-
-  phoneError(): string | null {
-    if (!this.tfno().trim()) return null;
-    return this.isValidPhone(this.tfno()) ? null : 'El teléfono debe tener 9 dígitos';
-  }
-
-  formularioValidado(): boolean {
-    return (
-      !!this.nombre().trim() &&
-      this.provincia() != null &&
-      !!this.tfno().trim() &&
-      this.isValidPhone(this.tfno()) &&
-      !!this.email().trim() &&
-      this.isValidEmail(this.email()) &&
-      !!this.descripcion().trim() &&
-      !!this.direccion().trim()
-    );
-  }
 }
