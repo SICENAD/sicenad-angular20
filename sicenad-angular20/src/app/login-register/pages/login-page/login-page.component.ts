91c2c845dcffe973e67b543768d713fe9061c2f9
joseluispuentes82 <joseluispuentes@hotmail.com>
2025-09-04 13:09:29 +0200

    login y register pasados a reactiveforms

--- sicenad-angular20/src/app/login-register/pages/login-page/login-page.component.ts
+++ sicenad-angular20/src/app/login-register/pages/login-page/login-page.component.ts
@@ -1,14 +1,15 @@
-import { Component, signal, inject, computed } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { RoutesPaths } from '@app/app.routes';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { OrquestadorService } from '@services/orquestadorService';
 import { AuthStore } from '@stores/auth.store';
 import { IconosStore } from '@stores/iconos.store';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [FontAwesomeModule, RouterLink],
+  imports: [FontAwesomeModule, RouterLink, ReactiveFormsModule],
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
@@ -17,20 +18,34 @@ export class LoginComponent {
   private auth = inject(AuthStore);
   private iconos = inject(IconosStore);
   private orquestadorService = inject(OrquestadorService);
+  private fb = inject(FormBuilder);
+
   faHome = this.iconos.faHome;
   readonly routesPaths = RoutesPaths;
 
-  username = signal('');
-  password = signal('');
   feedback = signal('');
 
-  formularioValidado = computed(() =>
-    this.username().trim() !== '' &&
-    this.password().trim() !== ''
-  );
+  loginForm: FormGroup = this.fb.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+  });
 
-  login() {
-    this.orquestadorService.loginUsuario(this.username(), this.password()).subscribe({
+  // Getter para acceder fácilmente a los controles
+  get username() {
+    return this.loginForm.get('username');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+ login() {
+    if (this.loginForm.invalid) {
+      this.feedback.set('Por favor, completa todos los campos.');
+      return;
+    }
+    const { username, password } = this.loginForm.value;
+    this.orquestadorService.loginUsuario(username, password).subscribe({
       next: (res) => {
         this.feedback.set('');
         this.auth.getDatosSeguridad(res.token, res.username, res.rol);
--- sicenad-angular20/src/app/login-register/pages/register-page/register-page.component.ts
+++ sicenad-angular20/src/app/login-register/pages/register-page/register-page.component.ts
@@ -1,12 +1,14 @@
-import { Component, signal, computed, inject } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { RoutesPaths } from '@app/app.routes';
 import { OrquestadorService } from '@services/orquestadorService';
 import { UtilsStore } from '@stores/utils.store';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule, JsonPipe],
   templateUrl: './register-page.component.html',
   styleUrls: ['./register-page.component.css']
 })
@@ -14,60 +16,75 @@ export class RegisterComponent {
   private utils = inject(UtilsStore);
   private orquestadorService = inject(OrquestadorService);
   private router = inject(Router);
+  private fb = inject(FormBuilder);
 
   readonly routesPaths = RoutesPaths;
-  username = signal('');
-  password = signal('');
-  tfno = signal('');
-  email = signal('');
-  emailAdmitido = signal(false);
-  descripcion = signal('');
+  registerForm: FormGroup = this.fb.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+    tfno: ['', [Validators.required, Validators.pattern(/^[0-9]{9}$/)]],
+    email: ['', [Validators.required, Validators.email]],
+    emailAdmitido: [false],
+    descripcion: ['', Validators.required],
+  });
+
+  get username() {
+    return this.registerForm.get('username');
+  }
+
+  get password() {
+    return this.registerForm.get('password');
+  }
+
+  get tfno() {
+    return this.registerForm.get('tfno');
+  }
+
+  get email() {
+    return this.registerForm.get('email');
+  }
+
+  get descripcion() {
+    return this.registerForm.get('descripcion');
+  }
+
+  get emailAdmitido() {
+    return this.registerForm.get('emailAdmitido');
+  }
+
+
+
   passwordForRegisterFromUser = signal('');
 
-  // Validadores
-  private isValidEmail = (email: string) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-  private isValidPhone = (phone: string) => /^[0-9]{9}$/.test(phone);
-
-  emailError = computed(() => {
-    if (!this.email().trim()) return null;
-    return this.isValidEmail(this.email()) ? null : 'El correo no es válido';
-  });
-
-  phoneError = computed(() => {
-    if (!this.tfno().trim()) return null;
-    return this.isValidPhone(this.tfno()) ? null : 'El teléfono debe tener 9 dígitos';
-  });
-
-  formularioValidado = computed(() =>
-    this.username().trim() !== '' &&
-    this.password().trim() !== '' &&
-    this.tfno().trim() !== '' &&
-    this.isValidPhone(this.tfno()) &&
-    this.email().trim() !== '' &&
-    this.isValidEmail(this.email()) &&
-    this.descripcion().trim() !== ''
-  );
-
   async solicitudRegistro() {
+
+
+    if (this.registerForm.invalid) {
+      alert('Por favor, completa todos los campos correctamente.');
+      return;
+    }
+    const { username, password, tfno, email, emailAdmitido, descripcion } = this.registerForm.value;
     const passwordForRegister = this.utils.passwordForRegister();
     passwordForRegister == this.passwordForRegisterFromUser()
       ?
       (this.orquestadorService.registerUsuario(
-        this.username(),
-        this.password(),
-        this.tfno(),
-        this.email(),
-        this.emailAdmitido(),
-        this.descripcion(),
+        username,
+        password,
+        tfno,
+        email,
+        emailAdmitido,
+        descripcion,
         'Superadministrador'
       ).subscribe({
         next: (res) => {
+          this.passwordForRegisterFromUser.set('');
+          this.router.navigate([RoutesPaths.login])
           console.log('Registro correcto:', res);
         },
         error: (err) => {
           console.error('Error en registro:', err);
         }
-      }), this.router.navigate([RoutesPaths.login])
+      })
       )
       : alert('El password introducido no es correcto');
   }
