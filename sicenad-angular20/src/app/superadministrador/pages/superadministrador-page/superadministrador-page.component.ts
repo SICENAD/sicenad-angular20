bfba36e72d7eaec5791a87c374a0385cba53757f
joseluispuentes82 <joseluispuentes@hotmail.com>
2025-09-04 20:40:18 +0200

    vista de superadministrador

--- sicenad-angular20/src/app/superadministrador/pages/superadministrador-page/superadministrador-page.component.ts
+++ sicenad-angular20/src/app/superadministrador/pages/superadministrador-page/superadministrador-page.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { RoutesPaths } from '@app/app.routes';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IconosStore } from '@stores/iconos.store';
 
 @Component({
   selector: 'app-superadministrador-page',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, FontAwesomeModule],
   templateUrl: './superadministrador-page.component.html',
   styleUrls: ['./superadministrador-page.component.css'],
 })
-export class SuperadministradorPageComponent { }
+export class SuperadministradorPageComponent {
+  private iconoStore = inject(IconosStore);
+
+  faHome = this.iconoStore.faHome;
+  readonly routesPaths = RoutesPaths;
+
+}
