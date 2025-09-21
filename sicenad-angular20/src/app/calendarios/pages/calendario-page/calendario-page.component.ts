771452efefb612cd190b05b5e488f06006177748
joseluispuentes82 <joseluispuentes@hotmail.com>
2025-09-21 15:48:13 +0200

    preparando el modulo calendario

---
+++ sicenad-angular20/src/app/calendarios/pages/calendario-page/calendario-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calendario',
+  imports: [],
+  templateUrl: './calendario-page.component.html',
+  styleUrls: ['./calendario-page.component.css'],
+})
+export class CalendarioPageComponent { }
