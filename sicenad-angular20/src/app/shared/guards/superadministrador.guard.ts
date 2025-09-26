954f9f63f2595b59eb4d368844a1df209d31645a
joseluispuentes82 <joseluispuentes@hotmail.com>
2025-09-26 12:28:33 +0200

    corrigiendo guard

--- sicenad-angular20/src/app/shared/guards/superadministrador.guard.ts
+++ sicenad-angular20/src/app/shared/guards/superadministrador.guard.ts
@@ -4,7 +4,7 @@ import { RoutesPaths } from '@app/app.routes';
 import { RolUsuario } from '@interfaces/enums/rolUsuario.enum';
 import { AuthStore } from '@stores/auth.store';
 
-export const superadministradorGuard = (): CanMatchFn => () => {
+export const superadministradorGuard: CanMatchFn = () => {
     const auth = inject(AuthStore);
     const router = inject(Router);
     return auth.rol() === RolUsuario.Superadministrador ? true : (alert(`Debes ser ${RolUsuario.Superadministrador} para acceder a esta página`), router.createUrlTree([RoutesPaths.home]));
