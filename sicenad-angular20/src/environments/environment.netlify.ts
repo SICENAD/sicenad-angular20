b35cc1852f94fb65a4ce981cf24008625e9fb312
joseluispuentes82 <joseluispuentes@hotmail.com>
2025-10-10 13:28:06 +0200

    corrijo environment

--- sicenad-angular20/src/environments/environment.netlify.ts
+++ sicenad-angular20/src/environments/environment.netlify.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
-    production: true,
+    production: false,
     publicPath: '/sicenad/', // aquí puedes poner el path de tu app en producción
     urlApi: 'https://puentes-sicenad-cda28931791a.herokuapp.com/api',
     forceJsonHeader: true, // en Netlify+Heroku
